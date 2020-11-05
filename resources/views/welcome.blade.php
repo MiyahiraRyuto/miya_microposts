edaeec2dca27855de15e356ce757b5088708c7a2
Ryuto.Miyahira <gyakusyuyou.miya@gmail.com>
2020-11-05 12:01:24 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <ht>Welcome to the Microposts!!!</ht>
+            {!! link_to_route('signup.get','Sign up now',[],['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection    
