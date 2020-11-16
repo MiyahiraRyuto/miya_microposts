bf21b2db9dd699ef4176ad1019f3850f79108030
Ryuto.Miyahira <gyakusyuyou.miya@gmail.com>
2020-11-16 13:33:25 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,14 +18,50 @@ class UsersController extends Controller
             'users' => $users,
         ]);
     }
-    public function show($id)
+       public function show($id)
     {
         // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
 
-        // ユーザ詳細ビューでそれを表示
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        // ユーザ詳細ビューでそれらを表示
         return view('users.show', [
             'user' => $user,
+            'microposts' => $microposts,
+        ]);
+    }
+        public function followings($id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロー一覧を取得
+        $followings = $user->followings()->paginate(10);
+
+        return view('users.followings', [
+            'user' => $user,
+            'users' => $followings,
+        ]);
+    }
+    public function followers($id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->loadRelationshipCounts();
+
+        $followers = $user->followers()->paginate(10);
+
+        return view('users.followers', [
+            'user' => $user,
+            'users' => $followers,
         ]);
     }
 }
+
+
